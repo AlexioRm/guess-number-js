8602ec7a426d03db98ae27cb0215c899dbee3d89
Alexio Rojas M. <alexiosebastianrm@gmail.com>
2025-05-15 18:26:47 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let nombre = prompt("¿Cómo te llamas?");
+let edad = prompt("¿Cuántos años tienes?");
+let ciudad = prompt("¿Dónde vives?");
+alert("Hola " + nombre + ", tienes " + edad + " años y vives en " + ciudad + ".");
+console.log("Nombre ingresado:", nombre);
+console.log("Edad ingresada:", edad);
+console.log("Ciudad ingresada:", ciudad);
